2ec17438f8a2e614ff0cdee25824cf0c80906dcf
joywinmissier <joywinmissier@gmail.com>
2021-09-19 13:51:16 +0530

    Auto-generated commit [ci skip]

--- new-lib-demo/lib/utils/sort.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Sort {
-    private sortOrder;
-    private collator;
-    constructor();
-    startSort(property: any, order: any, type?: string): (a: any, b: any) => number;
-    recursiveSort(listToSort: any, sortingProperty: any, sortOrder: any): void;
-}
